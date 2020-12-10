1571617e81e5ac14d35c2328c9e6a215900ea64f
Soohyun Lim <d2axasap@gmail.com>
2020-12-10 13:58:25 -0500

    Added cors dependency to handle GET requests coming in from localhost

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const db = require('../db-postgres/index.js');
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const router = require('./routes.js');

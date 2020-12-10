6d0d33a484f188a285c9ca071e84e15da2da85c3
Soohyun Lim <d2axasap@gmail.com>
2020-12-10 17:21:34 -0500

    Added loader.io (cloud-based stress test) and newrelic(monitoring platform) to the codebase

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+const newRelic = require('newrelic');
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 var cors = require('cors')
 const app = express();
 const db = require('../db-postgres/index.js');
@@ -11,8 +13,16 @@ app.use(bodyParser.json());
 const router = require('./routes.js');
 app.use('/', router);
 
+app.get('/loaderio-e1cde7e76e8c781dd7a81444a26c4d61', (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname, '../loaderio-e1cde7e76e8c781dd7a81444a26c4d61.txt'
+    )
+  );
+});
+
 app.get('*', (req, res) => {
   res.send('Page Not Found');
-})
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
